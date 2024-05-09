2ac8a6de3b1c21efedd90c103688068f9050802b
Tim <tim.kochs@hotmail.com>
2024-05-09 15:54:41 +0200

    added quicksort impl in zig

--- sorting/zig/src/main.zig
+++ sorting/zig/src/main.zig
@@ -11,6 +11,14 @@ pub fn main() !void {
     merge_sort(arr);
     std.debug.print("Sorted:\n", .{});
     display(arr);
+
+    const arr2 = make_random_list(5);
+    defer allocator.free(arr2);
+    std.debug.print("\nQuickSort\nUnsorted:\n", .{});
+    display(arr2);
+    quick_sort(arr2);
+    std.debug.print("Sorted:\n", .{});
+    display(arr2);
 }
 
 pub fn display(arr: []u8) void {
@@ -43,8 +51,8 @@ pub fn merge_sort(arr: []u8) void {
 pub fn merge(left: []u8, right: []u8) void {
     const merged: []u8 = allocator.alloc(u8, left.len) catch unreachable;
     defer allocator.free(merged);
-    var i: u16 = 0;
-    var j: u16 = 0;
+    var i: usize = 0;
+    var j: usize = 0;
     for (0..left.len) |k| {
         if ((j >= right.len) or (i < (left.len + 1) / 2 and left[i] < right[j])) {
             merged[k] = left[i];
@@ -59,7 +67,33 @@ pub fn merge(left: []u8, right: []u8) void {
     }
 }
 
-test "simple test" {
+pub fn swap(i: usize, j: usize, arr: []u8) void {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+pub fn quick_sort(arr: []u8) void {
+    if (arr.len <= 1) {
+        return;
+    }
+    const pivot = arr[0];
+    var lesser: usize = 1;
+    var equal: usize = 0;
+    for (1..arr.len) |i| {
+        if (arr[i] < pivot) {
+            swap(i, lesser, arr);
+            lesser += 1;
+        } else if (arr[i] == pivot) {
+            equal += 1;
+        }
+    }
+    swap(0, lesser - 1, arr);
+    quick_sort(arr[0 .. lesser - 1]);
+    quick_sort(arr[lesser + equal .. arr.len]);
+}
+
+test "test merge sort" {
     var unsorted = [_]u8{ 5, 3, 29, 17, 0 };
     const sorted = [_]u8{ 0, 3, 5, 17, 29 };
     merge_sort(&unsorted);
@@ -67,4 +101,14 @@ test "simple test" {
         std.debug.assert(sorted[i] == unsorted[i]);
     }
     std.debug.print("MergeSort successfully tested on an array of length 5!\n", .{});
+}
+
+test "test quick sort" {
+    var unsorted = [_]u8{ 5, 3, 29, 17, 0 };
+    const sorted = [_]u8{ 0, 3, 5, 17, 29 };
+    quick_sort(&unsorted);
+    for (0..sorted.len) |i| {
+        std.debug.assert(sorted[i] == unsorted[i]);
+    }
+    std.debug.print("QuickSort successfully tested on an array of length 5!\n", .{});
 }
